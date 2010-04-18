bc3c399477306ae39429a335a55ffd05a290c92e
gduperon <gduperon@5d9ba3ac-444b-4713-9fb3-0b58e79229a2>
2010-04-18 19:12:39 +0000

    IDE en javascript, premier commit : log.
    git-svn-id: https://projetud.info-ufr.univ-montp2.fr/svn/flin607-2009-gduperon@15 5d9ba3ac-444b-4713-9fb3-0b58e79229a2

---
+++ jside/ide.js
@@ -0,0 +1,8 @@
+function init() {
+    log("Démarré.");
+    log("Ajoutez des blocs à l'espace de travail pour construire un programme.");
+}
+
+function log(msg) {
+    $('#log-content').append("<p>"+msg+"</p>");
+}
